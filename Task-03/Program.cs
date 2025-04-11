32d52327975d9a18cb2f3e47ba930c3dd83703c3
Moumitha22 <moumitharaghu22@gmail.com>
2025-04-11 21:46:24 +0530

    Add task-3 Basic Collections and String Manipulation

---
+++ Task-03/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>();
+
+            bool exit = false;
+
+            Console.WriteLine("------------ Student Management ------------");
+            while (!exit)
+            {
+                Console.WriteLine("\nChoose an option :");
+                Console.WriteLine("1 -> Add new name to the list.");
+                Console.WriteLine("2 -> Remove existing name from the list.");
+                Console.WriteLine("3 -> Display the list.");
+                Console.WriteLine("4 -> Exit.\n");
+
+                int.TryParse(Console.ReadLine(), out int choice);
+
+                string name = string.Empty;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("\nEnter a name to add to the list : ");
+                        name = Console.ReadLine()?.Trim().ToUpper();
+                        if (!string.IsNullOrWhiteSpace(name))
+                        {
+                            students.Add(name);
+                            Console.WriteLine($"{name} was added to the list.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input.");
+                        }
+                        break;
+                        
+                    case 2:
+                        Console.Write("\nEnter the name to remove from the list : ");
+                        name = Console.ReadLine()?.Trim().ToUpper();
+                        if (students.Contains(name))
+                        {
+                            students.Remove(name);
+                            Console.WriteLine($"{name} was removed from the list.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The list doesn't contain the name {name}");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("---------- Students ----------");
+                        if (students.Count > 0)
+                        {
+                            foreach (string student in students)
+                            {
+                                Console.WriteLine(student);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("There are no names to display.");
+                        }
+                        break;
+
+                    case 4:
+                        exit = true;
+                        Console.WriteLine("Exiting...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter a valid choice.");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Task-03/README.md
@@ -0,0 +1,35 @@
+# Tssk 3 **Basic Collections and String Manipulation**
+## **Objective :**
+- Write a program to manage a list of strings (e.g., names or tasks).
+
+## **Requirements:**
+- Use a `List<string>` to store items.
+- Allow the user to add, remove, and display items.
+- Utilize loops and basic string methods (like `Trim()`, `ToUpper()`) to process user input.
+
+## **Concepts Used:**
+- Namespaces → `System.Collections.Generic` for Lists (`List<string>`)
+- Loops (`while`, `foreach`)
+- Conditional logic (`switch-case`)
+- String methods (`Trim()`, `ToUpper()`)
+
+## **Features:**
+- Add names with validation
+- Remove names only if they exist
+- Display all current names
+- Graceful handling of invalid inputs
+- Display of the list
+
+## **Sample Output:**
+
+#### Add names:
+
+![Add](./output1.png)
+
+#### Remove & Display names:
+
+![Remove & Display](./output2.png)
+
+#### Invalid Input:
+
+![Invalid-Input](./output3.png)
